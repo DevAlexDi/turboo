d1fe1c0f11fe01988dd38a10b31191904e6b46b4
Tatyana Komarova <komarovats93@gmail.com>
2018-01-25 11:56:04 +0300

    Валидация и отправка формы

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+$letter = '<html><body>';
+$letter = '<p>Клиент воспользовался формой на сайте <a href="http://'.$_SERVER['HTTP_HOST'].'" target="_blank">"Ремонт турбин 24"</a>.</p>';
+$letter .= '<p><strong>ФИО:</strong> '.$name.'</p>';
+$letter .= '<p><strong>Телефон:</strong> '.$phone.'</p>';
+$letter .= '</body></html>';
+
+$res['sent'] = false;
+if (mail('mail@remontturbin-24.ru', 'Заявка клиента | Ремонт турбин 24', $letter, 'Content-type: text/html; charset=utf-8')) $res['sent'] = true;
+return json_encode($res);
+
+
+?>
